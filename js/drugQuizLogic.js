5ec895484cd9d666ea5ffe891b36be5b6b4d62ec
josh <joshuahansolkim@gmail.com>
2018-04-16 18:22:03 -0700

    Added IPDM Drug List Quiz
    Updated logic for drug list quiz: added question list (pagination), added button for “correct answer.”

--- js/drugQuizLogic.js
+++ js/drugQuizLogic.js
@@ -1,40 +1,60 @@
-function checkAnswer(){
-    var questionType = $('input[name="questionSelector"]:checked').val();
-    switch (questionType) {
-        case 'genericRadio':
-            if(drugSubset[arrayIndex].genericName.toLowerCase() == $('#genericInput').val().toLowerCase()){
-                totalCorrect++;
-                answerCorrect[arrayIndex] = "1";
-                $('#genericInput').val('');
-            } else {
-                alert("The correct answer is: " + drugSubset[arrayIndex].genericName);
-                $('#genericInput').val('');
+function createOptionList() {
+    var drugSubsetList = [];
+    for(d = 0; d < drugs.length; d++){
+        if(drugSubsetList.indexOf(drugs[d].indication) == -1 && drugs[d].indication.length != 0){
+            drugSubsetList.push(drugs[d].indication);
+        }
+    }
+    for(s = 0; s < drugSubsetList.length; s++){
+        $('#quizOptions').append($('<option>', {
+        value: drugSubsetList[s],
+        text: drugSubsetList[s]
+        }));
+    }
+}
+
+function createDrugSubset(subset) {
+    if(subset == "all"){
+        drugSubset = drugs;
+    } else {
+        drugSubset = [];
+        for(d = 0; d < drugs.length; d++){
+            if(drugs[d].indication.toLowerCase() == subset.toLowerCase()){
+                drugSubset.push(drugs[d]);
             }
-            break;
-        case 'tradeRadio':
-            if(drugSubset[arrayIndex].tradeName.toLowerCase() == $('#tradeInput').val().toLowerCase()){
-                totalCorrect++;
-                answerCorrect[arrayIndex] = "1";
-                $('#tradeInput').val('');
-            } else {
-                alert("The correct answer is: " + drugSubset[arrayIndex].tradeName);
-                $('#tradeInput').val('');
-            }
-            break;
+        }
     }
     
-    $('#correctCount').val(totalCorrect + "/" + totalDrugs);
-    if(totalCorrect == totalDrugs) {
-        alert("Congratulations! You got them all right!");
-        location.reload();
-    } else {
-        getNewQuestion();
+    totalCorrect = 0;
+    totalDrugs = drugSubset.length;
+    getNewQuestion();
+    printDrugList();
+}
+
+function setupPagination() {
+	$('.removeable').remove();
+    for(num in drugSubset){
+        var questionNum = parseInt(num) + 1;
+        $('.questionNav').last().after('<li class="page-item questionNav"><button class="page-link removeable" type="button" onclick="getQuestion('+ num +')" id="question'+num+'">' + questionNum + '</button></li>');
     }
 }
 
 function getNewQuestion () {
-    getNewQuestionNumber();
+    /* Bug fix for arrayindex not getting set correctly on app start. */
+    if(arrayIndex > 0){
+        if($('#question'+arrayIndex).attr('class').includes('answerCorrect')){
+            arrayIndex++;
+            getNewQuestion();
+        }
+    }
+    /* Change pagination to show which question is active. */
+    $(".page-link").parent().removeClass('active');
+    $('#question'+arrayIndex).parent().addClass('active');
+    
+    /* Get drug for question */
     var drug = drugSubset[arrayIndex];
+    
+    /* Determine what the drug data type that should be asked for and populate other fields. */
     $('.userInput').attr('disabled', true);
     if($('#tradeRadio').prop('checked')){
         $('#tradeInput').attr('value', '').attr('disabled', false);
@@ -72,47 +92,131 @@ function getNewQuestion () {
         $('#exclusionsInput').text(drug.exclusions);
     }
     
-}
-
-function getNewQuestionNumber () {
-    arrayIndex = Math.floor((Math.random() * drugSubset.length));
-    if (answerCorrect[arrayIndex] == 1){
-        getNewQuestionNumber();
-    }
-    console.log("Question Number: " + arrayIndex);
-}
-
-function createOptionList() {
-    var drugSubsetList = [];
-    for(d = 0; d < drugs.length; d++){
-        if(drugSubsetList.indexOf(drugs[d].indication) == -1 && drugs[d].indication.length != 0){
-            drugSubsetList.push(drugs[d].indication);
+    /* This modifies the Info box below the question depending on whether previous questions have been answered */
+    var questionType = $(".questionSelector").val();
+    if("input" in drugSubset[arrayIndex]){
+        $('#correctAnswerLabel').text('Previously Entered Answer:');
+        switch (questionType) {
+        case 'genericRadio':
+            $('#correctAnswer').val(drugSubset[arrayIndex].genericName);
+            break;
+        case 'tradeRadio':
+            $('#correctAnswer').val(drugSubset[arrayIndex].tradeName);
+            break;
         }
-    }
-    for(s = 0; s < drugSubsetList.length; s++){
-        $('#quizOptions').append($('<option>', {
-        value: drugSubsetList[s],
-        text: drugSubsetList[s]
-        }));
-    }
-}
-
-function createDrugSubset(subset) {
-    if(subset == "all"){
-        drugSubset = drugs;
+	    
+    } else if(arrayIndex > 0 && "input" in drugSubset[arrayIndex - 1]){
+	    $('#correctAnswerLabel').text('Correct Answer to Previous Question:');
+        switch(questionType) {
+            case 'genericRadio':
+                $('#correctAnswer').val(drugSubset[arrayIndex-1].genericName);
+                break;
+            case 'tradeRadio':
+                $('#correctAnswer').val(drugSubset[arrayIndex-1].tradeName);
+                break;
+        }
     } else {
-        drugSubset = [];
-        for(d = 0; d < drugs.length; d++){
-            if(drugs[d].indication.toLowerCase() == subset.toLowerCase()){
-                drugSubset.push(drugs[d]);
+	    $('#correctAnswerLabel').text('Continue Quiz:');
+	    $('#correctAnswer').val('Press "Check Answer" or "Enter" key to submit your answer.');
+    }   
+}
+
+function getQuestion(index){
+    arrayIndex = index;
+    getNewQuestion();
+}
+
+function getNextQuestion() {
+    if(arrayIndex < drugSubset.length - 1){
+	    arrayIndex++;
+	    getNewQuestion();
+    } else {
+        //alert("You have reached the end of the quiz.");
+        getFirstIncorrectQuestion()
+    }
+}
+
+function getFirstQuestion() {
+    arrayIndex = 0;
+    getNewQuestion();
+}
+
+function getFirstIncorrectQuestion(){
+    var firstIncorrect = $('.incorrect').first().attr('id');
+    arrayIndex = parseInt(firstIncorrect.replace("question",""));
+    getNewQuestion();
+}
+
+function randomizeQuestions() {
+    var arrayIndex = 0;
+	drugSubset.sort(function(a, b){return 0.5 - Math.random()});
+	setupPagination();
+    getFirstQuestion();
+}
+
+function checkAnswer(){
+    var questionType = $('input[name="questionSelector"]:checked').val();
+    switch (questionType) {
+        case 'genericRadio':
+            if(drugSubset[arrayIndex].genericName.toLowerCase() == $('#genericInput').val().toLowerCase()){
+                if(drugSubset[arrayIndex].correct != "1"){
+                    totalCorrect++;
+                }
+                drugSubset[arrayIndex].correct = "1";
+	            $('#correctAnswer').removeClass('is-invalid').addClass('is-valid');
+                $("#question" + arrayIndex).removeClass('incorrect').addClass('answerCorrect');
+            } else {
+                $("#question" + arrayIndex).addClass('incorrect');
+                console.log("incorrect");
             }
-        }
+            $("#genericInput").val('');
+            drugSubset[arrayIndex].input = $('#genericInput').val();
+            break;
+        case 'tradeRadio':
+            if(drugSubset[arrayIndex].tradeName.toLowerCase() == $('#tradeInput').val().toLowerCase()){
+                if(drugSubset[arrayIndex].correct != "1"){
+                    totalCorrect++;
+                }
+                drugSubset[arrayIndex].correct = "1";
+                $("#question" + arrayIndex).removeClass('incorrect').addClass('answerCorrect');
+            } else {
+                $("#question" + arrayIndex).addClass('incorrect');
+                console.log("incorrect");
+            }
+            $("#tradeInput").val('');
+            drugSubset[arrayIndex].input = $('#tradeInput').val();
+            break;
     }
     
-    totalCorrect = 0;
-    totalDrugs = drugSubset.length;
-    getNewQuestion();
-    printDrugList();
+    $('#correctCount').text(totalCorrect + "/" + totalDrugs);
+    if(totalCorrect == totalDrugs) {
+        alert("Congratulations! You got them all right!");
+        location.reload();
+    } else {
+        getNextQuestion();
+    }
+}
+
+function getCorrectAnswer(){
+    $('#correctAnswerLabel').text('Correct Answer:');
+    var questionType = $('input[name="questionSelector"]:checked').val();
+    switch (questionType) {
+        case 'genericRadio':
+            $('#correctAnswer').val(drugSubset[arrayIndex].genericName);
+            break;
+        case 'tradeRadio':
+            $('#correctAnswer').val(drugSubset[arrayIndex].tradeName);
+            break;
+    }
+}
+
+function getPreviousQuestion() {
+    if(arrayIndex > 0){
+	    arrayIndex--;
+	    getNewQuestion();
+    } else {
+        alert("You have reached the beginning of the quiz.")
+    }
 }
 
 function printDrugList() {
@@ -123,5 +227,8 @@ function printDrugList() {
 }
 
 function resetScore() {
-    $('#correctCount').val("0/" + totalDrugs);
+    totalDrugs = drugSubset.length;
+    $('#correctCount').text("0/" + totalDrugs);
+    totalCorrect = 0;
+    randomizeQuestions();
 }
